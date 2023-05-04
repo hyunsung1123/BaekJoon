893f23553dd34cc92d0067a4faadd62ab21c80d0
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-04 15:02:04 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 204 ms, Memory: 48468 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -1,15 +1,14 @@
 import sys
 input = sys.stdin.readline
+
 n = int(input())
-A = [input().strip().split(" ") for i in range(n)]
-A_dict = dict(A)
-to_remove = []  # 삭제해야 할 원소의 인덱스를 저장할 리스트
-for idx,val in enumerate(A_dict):
-    if A_dict[val] == 'leave':
-        to_remove.append(val)
-for i in reversed(to_remove):  # 인덱스가 큰 원소부터 삭제해야 함
-    del A_dict[i]
-keys = list(A_dict.keys())
-keys.sort(reverse=True)
-for key in keys:
-    print(key)
+dict= {}
+for _ in range(n):
+    name,log = input().strip().split()
+    dict[name]=log
+    if log == 'leave':
+        del dict[name]
+dict_key = list(dict.keys())
+dict_key.sort(reverse=True)
+for i in dict_key:
+    print(i)
