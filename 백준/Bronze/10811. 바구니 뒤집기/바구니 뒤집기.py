2272735a8c5b09c4ba72f97e90b31433e39d847e
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-13 17:11:57 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -2,7 +2,7 @@ import sys
 input = sys.stdin.readline
 N,M = map(int,input().split())
 A = [i for i in range(1,N+1)]
-for i in range(M):
+for _ in range(M):
     i,j = map(int,input().split())
     A[i-1:j] = reversed(A[i-1:j])
 print(*A)
