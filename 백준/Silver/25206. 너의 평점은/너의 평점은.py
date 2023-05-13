51c05f4186ea2c38c4e22dca2caceae260d91bad
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-13 15:11:25 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,13 +1,13 @@
-rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
-grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+rate = 0
+scoreSum = 0
 
-total = 0	# 학점 총합을 담을 변수
-result = 0	# (학점 * 과목평점) 총합을 담을 변수
-for _ in range(20) :
-    s, p, g = input().split()
-    p = float(p)
-    if g != 'P' :	# 등급이 P인 과목은 계산 안함
-        total += p
-        result += p * grade[rating.index(g)]
+rating = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 
-print('%.6f' % (result / total))
+for i in range(20):
+    subject, score, grade = input().split()
+    if grade == "P":
+        continue
+    rate += float(score) * rating[grade]
+    scoreSum += float(score)
+    
+print(rate/scoreSum)
