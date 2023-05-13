e3415cdfa5dd1d51342ed3be564c18012ffb1525
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-13 15:25:14 +0900

    [Bronze II] Title: 공 바꾸기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+A = [i for i in range(1,N+1)]
+for _ in range(M):
+    i,j=map(int,input().split())
+    A[i-1],A[j-1]=A[j-1],A[i-1]
+print(*A)
