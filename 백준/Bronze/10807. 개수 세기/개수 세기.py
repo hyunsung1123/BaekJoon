725e3522b68fda1de9c5b6add129042168b5a014
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-13 14:41:47 +0900

    [Bronze V] Title: 개수 세기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.py
+++ 백준/Bronze/10807. 개수 세기/개수 세기.py
@@ -4,8 +4,4 @@ input = sys.stdin.readline
 T = int(input())
 A = list(map(int,input().strip().split()))
 v = int(input())
-count=0
-for i in A:
-    if i == v:
-        count+=1
-print(count)
+print(A.count(v))
