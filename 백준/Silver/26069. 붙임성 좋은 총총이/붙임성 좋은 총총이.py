99b42b97cf90611be15d5a5b7c144e5353dbaedd
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-05-14 10:27:24 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -2,13 +2,15 @@ import sys
 input = sys.stdin.readline
 
 N=int(input())
-A=["ChongChong"]
+A=set()
+A.add("ChongChong")
 sum=0
 for _ in range(N):
     human1,human2=input().strip().split()
     if human1 in A:
-        A.append(human2)
+        A.add(human2)
     if human2 in A:
-        A.append(human1)
-print(len(set(A)))
+        A.add(human1)
+print(len(A))
+
 
