d7e2074f6535e740c477e8736a2bbbd97eb13df7
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-07-07 18:01:21 +0900

    [unrated] Title: 달리기 경주, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def solution(players, callings):
+    player_indices = {player: index for index, player in enumerate(players)} 
+    
+    callings_queue = deque(callings)
+    while callings_queue:
+        call = callings_queue.popleft()
+        index = player_indices.get(call)  
+        if index is not None:
+            players[index-1], players[index] = players[index], players[index-1]
+            player_indices[players[index]] = index
+            player_indices[players[index-1]] = index - 1
+
+    return players
