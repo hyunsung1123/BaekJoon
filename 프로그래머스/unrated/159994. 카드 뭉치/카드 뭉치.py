315da955f7c693a6ec46e3d22df7437af35b7a57
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-07-09 20:18:39 +0900

    Update 카드 뭉치.py

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -5,22 +5,26 @@ def solution(cards1, cards2, goal):
     cards2 = deque(cards2)
     goal = deque(goal)
     flag=True #만들어질수있는지 검사하는 Flag
-    while goal:
+    
+    while goal: #goal에 찾을 값이 남아있을때까지 반복.
+        find_item = goal.popleft()
         if cards1:
             cards1_item = cards1.popleft()
-        find_item = goal.popleft()
         if cards2:
             cards2_item = cards2.popleft()
-        print(find_item,cards1_item,cards2_item)
-        if find_item == cards1_item:
-            cards2.appendleft(cards2_item)
-        elif find_item == cards2_item:
-            cards1.appendleft(cards1_item)
-        else:
+
+        if find_item == cards1_item: #card1의 처음원소가 원하는 원소인경우.
+            cards2.appendleft(cards2_item) #card2의 원소는 다시 맨앞에 추가.
+            
+        elif find_item == cards2_item: #card2의 처음원소가 원하는 원소인경우
+            cards1.appendleft(cards1_item) #card1의 원소는 다시 맨앞에 추가
+            
+        else: #맨앞에 있는게 둘다 아닌경우 -> 문장성립X -> flag를 false로 설정 후 break
             flag=False
             break
     if flag:
         answer="Yes"
     else:
         answer="No"
+        
     return answer
