d7a7bdd5a6def265c8b0cff2cbf0578d0eab2e62
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-07-12 15:45:15 +0900

    [unrated] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/unrated/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,19 +2,8 @@ def solution(k, score):
     answer = []
     result = []
     for jumsu in score:
-        if len(result)+1<=k:
-            result.append(jumsu)
-            result.sort(reverse=True)
-            answer.append(result[-1])
-            
-        else:
-            low_score = result.pop()
-            
-            if low_score < jumsu:
-                result.append(jumsu)
-            else:
-                result.append(low_score)
-                
-            result.sort(reverse=True)
-            answer.append(result[-1])
+        result.append(jumsu)
+        if len(result)>k:
+            result.remove(min(result))
+        answer.append(min(result))
     return answer
