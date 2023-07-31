638a39494ae60f3a3d699f7ae5ea0bb3b14eca30
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-07-31 19:36:39 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,20 +1,14 @@
 import sys
 input = sys.stdin.readline
-
-# 자연수 N,M ->1~N 중에서 길이가 M 인 수열 구하기.
-
-
-N, M = list(map(int, input().split()))
+N,M = map(int,input().split(" "))
 result = []
-check = []
-def dfs():
-    if len(result) == M:
+def dfs(depth,start):
+    if depth == M:
         print(*result)
         return
-    for i in range(1, N+1):
-        if i not in result:
-            if all(i >= j for j in result):  
-                result.append(i)
-                dfs()
-                result.pop()
-dfs()
+    for i in range(start,N+1):
+        result.append(i)
+        dfs(depth+1,i+1)
+        result.pop()
+
+dfs(0,1)
