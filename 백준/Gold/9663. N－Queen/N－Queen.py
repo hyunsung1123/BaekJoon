4bce2f39e7df1278fe14c2250d1022d980a909f1
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-08-02 12:14:27 +0900

    [Gold IV] Title: N-Queen, Time: 29784 ms, Memory: 206344 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,22 +1,25 @@
+import sys
+input = sys.stdin.readline
+# N-QUEEN
+# 일차원 배열을 만들어 해당 일차원배열에 값과 다른값을 비교해서 퀸을 놓을수 있는지 없는지 판단
 def is_promising(x):
     for i in range(x):
-        if row[x] == row[i] or abs(row[x]-row[i]) == abs(x - i):
+        if row[x] == row[i] or x-i == abs(row[x]-row[i]): #x보다 작은 row의 값들을 비교하여 같은 열에 있는지와 대각선에 있는지 검사.
             return False
     return True
-def n_queens(x):
+    
+def dfs(x):
     global result
-    if x == N: #탈출 조건. 놓을 말이 없는 경우
-        #print(row)
+    if x==N:
         result +=1
         return
     else:
         for i in range(N):
-            row[x] = i
-            if is_promising(x):
-                n_queens(x+1)
-
+            row[x] = i # x행에 i번째 열에 퀸을 놓겠단 의미.
+            if is_promising(x): #row[x]가 허용된다면 x에 1을 증가시켜 dfs 재귀호출.
+                dfs(x+1)
 N = int(input())
 row = [0] * N
-result = 0
-n_queens(0)
+result = 0  # 퀸을 놓을 수있는 경우의수를 체크하기위한 변수
+dfs(0)
 print(result)
