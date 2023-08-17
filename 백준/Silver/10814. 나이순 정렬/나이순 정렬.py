01cc12c4de9e78d025079e277339645303230268
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2023-08-17 22:39:55 +0900

    [Silver V] Title: 나이순 정렬, Time: 264 ms, Memory: 49108 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,10 +1,10 @@
 import sys
-N = int(sys.stdin.readline())
-member=[]
-for i in range(N):
-    age,name=map(str,sys.stdin.readline().split())
-    age = int(age)
-    member.append((age,name))
-member.sort(key = lambda x : x[0])
-for i in range(N):
-    print(member[i][0],member[i][1])
+input = sys.stdin.readline
+N = int(input())
+A = []
+for i in range(0,N):
+    a,b = input().split()
+    A.append([int(a),b])
+A = sorted(A, key=lambda n : n[0])
+for i in A:
+    print(*i) # 리스트 안에 리스트 형식을 풀어줌
