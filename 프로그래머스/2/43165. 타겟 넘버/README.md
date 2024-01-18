f37df31b7f866098f19cac0dfcdbd19568f5d7a5
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2024-01-18 13:44:06 +0900

    [level 2] Title: 타겟 넘버, Time: 6.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 2.92 ms
+메모리: 10.1 MB, 시간: 6.77 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 4일 13:37:23
+2024년 1월 4일 13:44:4
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,12 +1,12 @@
+import sys
+sys.setrecursionlimit(10**6)
 answer=0
 def solution(numbers, target):
     global answer
-    #숫자 방문처리사용할 리스트
-    visited = [False for _ in range(len(numbers))]
     dfs(0,0,target,numbers)
     return answer
 
-def dfs(depth,current,target,numbers):
+def dfs(depth,current,target,numbers,):
     global answer
     if depth == len(numbers):
         if current == target:
@@ -15,3 +15,4 @@ def dfs(depth,current,target,numbers):
     else:
         dfs(depth+1,current+numbers[depth],target,numbers)
         dfs(depth+1,current-numbers[depth],target,numbers)
+    
