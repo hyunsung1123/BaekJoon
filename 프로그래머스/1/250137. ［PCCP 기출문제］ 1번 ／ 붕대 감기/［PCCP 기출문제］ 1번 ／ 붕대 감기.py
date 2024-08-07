f91c882036b9b5436d119e2dd7ccfad6ed9ac36e
조현성 <88260430+hyunsung1123@users.noreply.github.com>
2024-08-07 22:17:48 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.49 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -0,0 +1,26 @@
+def solution(bandage, health, attacks):
+    answer = 0
+    time = 0
+    heal_time = 0
+    count = 0
+    max_health = health
+    i = 0
+    while i < len(attacks):
+        if health <= 0:
+            return -1
+        if attacks[i][0] == time:
+            health -= attacks[i][1]
+            i+=1
+            heal_time = 0
+        else:
+            heal_time += 1
+            if heal_time == bandage[0]:
+                health += bandage[2]
+                heal_time = 0
+            health += bandage[1]
+        if health > max_health:
+            health = max_health
+        time += 1
+    if health <= 0:
+        return -1
+    return health
